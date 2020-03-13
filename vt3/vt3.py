6f0512725621e259dc465576780e29344e9f0f6b
Jason Harris <sirmanlypowers@gmail.com>
2020-03-12 19:02:40 -0700

    Seeking signs of life.

--- vt3/vt3.py
+++ vt3/vt3.py
@@ -7,20 +7,27 @@ Driver for a VersaTenn III Environmental Chamber Controller
 #------------------------------------------------------------------------------
 
 import serial
+import time
 
 #------------------------------------------------------------------------------
 
-ENQ = 5 # Enquiry
-ACK = 6 # Acknowledge
-NAK = 21 # Neg. Acknowledge
-STX = 02 # Start of Text
-ETX = 03 # End of Text
-EOT = 04 # End of Transmission
-DLE = 16 # Data Link Escape
-LF = 10 # Line Feed
-CR = 13 # Carriage Return
-XON = 17 # X-On
-XOFF = 19 # X-Off
+ENQ = b'\x05' # Enquiry
+ACK = b'\x06' # Acknowledge
+NAK = b'\x15' # Neg. Acknowledge
+STX = b'\x02' # Start of Text
+ETX = b'\x03' # End of Text
+EOT = b'\x04' # End of Transmission
+DLE = b'\x10' # Data Link Escape
+LF = b'\x0a' # Line Feed
+CR = b'\x0d' # Carriage Return
+XON = b'\x11' # X-On
+XOFF = b'\x13' # X-Off
+
+#------------------------------------------------------------------------------
+
+def dump(msg, x):
+  s = ['%02x' % c for c in x]
+  print("%s: %s" % (msg, " ".join(s)))
 
 #------------------------------------------------------------------------------
 
@@ -42,6 +49,7 @@ class vt3:
 
   def command(self, cmd, rsp=True):
     """send a command, receive a response"""
+    dump("tx", cmd)
     # wait a minimal amount of time between commands
     if self.last_time is not None:
       delta = MINIMAL_TIME + self.last_time - time.time()
@@ -52,21 +60,16 @@ class vt3:
     self.serial.write(cmd)
     # get a response if required
     if rsp:
-      return self.serial.readline().strip()
+      x = self.serial.read_until(terminator=ACK)
+      dump("rx", x)
+      return x
     return None
 
+  def enquire(self):
+    rsp = self.command(b'0' + ENQ)
+
   def get_sw_version(self):
-    rsp = self.command(b'? MDL')
-
-
-
-
-
-
-
-
-
-
+    rsp = self.command(STX + b'? MDL' + ETX)
 
 
 #------------------------------------------------------------------------------
